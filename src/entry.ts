025d57da65bccc9e02dc4ddaebd3bb20cd3a3afa
Daiki Nakashita <daikinakashita@Daikiui-MacBookPro.local>
2020-11-11 13:29:55 +0900

    add CircularStatic

--- src/entry.ts
+++ src/entry.ts
@@ -1,4 +1,5 @@
 import CenterAlignChildren from "./lib/centerAlignChildren";
+import circularStatic from "./lib/circularStatic";
 import DialogAppBar from "./lib/styled/dialogAppBar";
 import StyledButton from "./lib/styled/styledButton";
 import StyledButtonGroup from "./lib/styled/styledButtonGroup";
@@ -9,6 +10,7 @@ import StyledTextField from "./lib/styled/styledTextField";
 
 const components = {
     CenterAlignChildren,
+    circularStatic,
     DialogAppBar,
     StyledButton,
     StyledButtonGroup,
---
+++ src/lib/circularStatic.tsx
@@ -0,0 +1,50 @@
+import Box from "@material-ui/core/Box";
+import CircularProgress, {
+    CircularProgressProps,
+} from "@material-ui/core/CircularProgress";
+import Typography from "@material-ui/core/Typography";
+import React, { useEffect } from "react";
+
+function CircularProgressWithLabel(
+    props: CircularProgressProps & { sec: number; value: number }
+) {
+    return (
+        <Box position="relative" display="inline-flex">
+            <CircularProgress variant="static" {...props} />
+            <Box
+                top={0}
+                left={0}
+                bottom={0}
+                right={0}
+                position="absolute"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Typography
+                    variant="caption"
+                    component="div"
+                    color="textSecondary"
+                >
+                    {props.sec} s
+                </Typography>
+            </Box>
+        </Box>
+    );
+}
+
+export default function CircularStatic() {
+    const [sec, setSec] = React.useState(3);
+    const progress: number = sec * 33;
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setSec((sec) => (sec <= 0 ? 3 : sec - 1));
+        }, 900);
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    return <CircularProgressWithLabel value={progress} sec={sec} />;
+}
