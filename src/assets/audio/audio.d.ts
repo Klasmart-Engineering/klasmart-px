b680a98deafbb6c3c9e2077999d806b491e1dc36
Daiki Nakashita <daikinakashita@Daikiui-MacBookPro.local>
2020-11-11 10:07:16 +0900

    add assets

---
+++ src/assets/audio/audio.d.ts
@@ -0,0 +1,5 @@
+declare module "*.m4a" { const data: string; export default data; }
+declare module "*.mp3" { const data: string; export default data; }
+declare module "*.wav" { const data: string; export default data; }
+declare module "*.flac" { const data: string; export default data; }
+declare module "*.aac" { const data: string; export default data; }
---
+++ src/assets/video/video.d.ts
@@ -0,0 +1,2 @@
+declare module "*.mp4" { const data: string; export default data; }
+declare module "*.mkv" { const data: string; export default data; }
