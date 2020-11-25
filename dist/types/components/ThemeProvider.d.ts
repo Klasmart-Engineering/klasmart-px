943f7209a81363bffb5a9fdb6d859da821f456f0
Henrik Wassdahl <henrik.wassdahl@calmid.com>
2020-11-25 19:12:31 +0900

    added carrots to package.json and removed import of css

--- dist/types/components/ThemeProvider.d.ts
+++ dist/types/components/ThemeProvider.d.ts
@@ -1,5 +1,2 @@
 import { Theme } from "@material-ui/core";
-import "node-source-han-sans-sc/SourceHanSansSC-Regular-all.css";
-import "typeface-nanum-square-round";
-import "../assets/styles/index.min.css";
 export default function ThemeProvider(languageCode: string, themeMode: string): Theme;
