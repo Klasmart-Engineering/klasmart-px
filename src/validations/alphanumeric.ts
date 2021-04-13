01d903b328730e6a1e3738e9e0e0a7659ab681a9
Henrik Wassdahl <henrik.wassdahl@calmid.com>
2021-04-13 04:30:28 +0000

    KL-4549 - Alphanumeric validation allows incorrect symbols
    * fix alphanumeric validation & add test to cover previous bug
    
    Approved-by: Shawn Lee

--- src/validations/alphanumeric.ts
+++ src/validations/alphanumeric.ts
@@ -1,3 +1,3 @@
-export const alphanumericRegex = /^\w*$/;
+export const alphanumericRegex = /^[^\W_]*$/;
 
 export default (errorMessage?: string) => (input: any) => alphanumericRegex.test(input ?? ``) || (errorMessage ?? `Only alphanumeric characters are valid`);
