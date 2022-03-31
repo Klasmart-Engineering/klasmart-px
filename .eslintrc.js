46d445b54772646616959da5d5bbd981d58c2ed0
Henrik Wassdahl <henrik.wassdahl@kidsloop.live>
2022-03-31 19:54:25 +0900

    ci: add more workflows

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
+// required to load local dependencies of kidsloop-eslint-config
+// https://github.com/eslint/eslint/issues/3458
+require(`@rushstack/eslint-patch/modern-module-resolution`);
+
 module.exports = {
     extends: [ `@kl-engineering/eslint-config/react`, `@kl-engineering/eslint-config/jest/react` ],
     parserOptions: {
