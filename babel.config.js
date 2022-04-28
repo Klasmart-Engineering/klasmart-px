a2e8a985259d32581ab99bdd7cbe13133b4bdf0b
Henrik Wassdahl <henrik.wassdahl@calmid.com>
2022-04-28 20:45:30 +0900

    fix: tsc config

--- babel.config.js
+++ babel.config.js
@@ -19,5 +19,24 @@ module.exports = {
             },
         ],
     ],
-    plugins: [],
+    plugins: [
+        [
+            `babel-plugin-import`,
+            {
+                libraryName: `@mui/material`,
+                libraryDirectory: ``,
+                camel2DashComponentName: false,
+            },
+            `core`,
+        ],
+        [
+            `babel-plugin-import`,
+            {
+                libraryName: `@mui/icons-material`,
+                libraryDirectory: ``,
+                camel2DashComponentName: false,
+            },
+            `icons`,
+        ],
+    ],
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,54 @@
 import pkg from './package.json';
+// import babel from "@rollup/plugin-babel";
 // import alias from '@rollup/plugin-alias';
 import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import esbuild from 'rollup-plugin-esbuild';
+import dts from "rollup-plugin-dts";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+// import esbuild from 'rollup-plugin-esbuild';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 // import replace from '@rollup/plugin-replace';
 // import babel from "rollup-plugin-babel";
 // import nodePolyfills from 'rollup-plugin-node-polyfills';
 import postCSS from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
-import typescript2 from 'rollup-plugin-typescript2';
+// import typescript2 from 'rollup-plugin-typescript2';
 
-export default {
-    input: `src/index.ts`,
-    // output file names are loaded from the `package.json` main and module entries
-    output: [
-        {
-            file: pkg.main,
-            format: `cjs`,
-            sourcemap: true,
-        },
-        {
-            file: pkg.module,
-            format: `es`,
-            sourcemap: true,
-        },
-    ],
-    // this marks all peerDependencies as external meaning they won't be bundled in the distribution
-    external: [ ...Object.keys(pkg.peerDependencies || {}) ],
-    plugins: [
-        typescript(),
-        nodePolyfills({
-            include: [`src/**/*.ts`, `src/**/*.tsx`],
-        }),
-        nodeResolve({
-            preferBuiltins: false
-        }),
-        commonjs(),
-        postCSS(),
-        terser(),
+export default [
+    {
+        input: `src/index.ts`,
+        // output file names are loaded from the `package.json` main and module entries
+        output: [
+            {
+                file: pkg.main,
+                format: `cjs`,
+                sourcemap: true,
+            },
+            {
+                file: pkg.module,
+                format: `es`,
+                sourcemap: true,
+            },
+        ],
+        // this marks all peerDependencies as external meaning they won't be bundled in the distribution
+        external: [ ...Object.keys(pkg.peerDependencies || {}) ],
+        plugins: [
+            peerDepsExternal(),
+            nodePolyfills({
+                include: [ `src/**/*.ts`, `src/**/*.tsx` ],
+            }),
+            nodeResolve(),
+            // babel({
+            //     babelHelpers: `runtime`,
+            //     exclude: `node_modules/**`,
+            // }),
+            typescript({
+                tsconfig: `./tsconfig.json`,
+            }),
+            commonjs(),
+            postCSS(),
+            terser(),
         // typescript(),
         // nodeResolve({
         //     preferBuiltins: false
@@ -54,31 +63,43 @@ export default {
         //     include: null,
         // }),
         // postCSS(),
-    ],
-    // plugins: [
-    //     nodePolyfills(),
-    //     // babel({
-    //     //     exclude: /node_modules/,
-    //     //     presets: [
-    //     //         `@babel/preset-env`,
-    //     //         `@babel/preset-react`,
-    //     //         `@babel/preset-typescript`,
-    //     //     ],
-    //     // }),
-    //     postCSS({
-    //         plugins: [],
-    //         minimize: true,
-    //     }),
-    //     typescript({
-    //         useTsconfigDeclarationDir: true,
-    //     }),
-    //     nodeResolve(),
-    //     commonjs(),
-    //     // replace({
-    //     //     preventAssignment: true,
-    //     // }),
-    //     terser({
+        ],
+        // plugins: [
+        //     nodePolyfills(),
+        //     // babel({
+        //     //     exclude: /node_modules/,
+        //     //     presets: [
+        //     //         `@babel/preset-env`,
+        //     //         `@babel/preset-react`,
+        //     //         `@babel/preset-typescript`,
+        //     //     ],
+        //     // }),
+        //     postCSS({
+        //         plugins: [],
+        //         minimize: true,
+        //     }),
+        //     typescript({
+        //         useTsconfigDeclarationDir: true,
+        //     }),
+        //     nodeResolve(),
+        //     commonjs(),
+        //     // replace({
+        //     //     preventAssignment: true,
+        //     // }),
+        //     terser({
 
     //     }),
     // ],
-};
+    },
+    // {
+    //     input: `dist/types/index.d.ts`,
+    //     output: [
+    //         {
+    //             file: `dist/index.d.ts`,
+    //             format: `esm`,
+    //         },
+    //     ],
+    //     plugins: [ dts() ],
+    //     external: [ /\.css$/ ],
+    // },
+];
